f63a6fced27fdca2b0394b0da03571d0a5f3fe94
Mark Silverberg <mark.silverberg@socrata.com>
2016-01-16 17:16:20 -0500

    stop geocoder from ruining the fun

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,9 +1,9 @@
 Geocoder.configure(
   lookup: :google,
   always_raise: [
-    Geocoder::OverQueryLimitError,
-    Geocoder::RequestDenied,
-    Geocoder::InvalidRequest,
-    Geocoder::InvalidApiKey
+    # Geocoder::OverQueryLimitError,
+    # Geocoder::RequestDenied,
+    # Geocoder::InvalidRequest,
+    # Geocoder::InvalidApiKey
   ]
 )
